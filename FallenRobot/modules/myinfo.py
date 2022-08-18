c044a1d200362ffb8d033d1aadfcdf0935fe7f12
TEZZA <93602145+Tezzarobot@users.noreply.github.com>
2022-08-18 19:49:32 +0530

    Update myinfo.py

--- FallenRobot/modules/myinfo.py
+++ FallenRobot/modules/myinfo.py
@@ -16,13 +16,13 @@ from FallenRobot import dispatcher
 
 
 edit_time = 5
-""" =======================ZEN ROBOT====================== """
-file1 = "https://telegra.ph/file/90552395a5e96d0e7fab9.jpg"
-file2 = "https://telegra.ph/file/90552395a5e96d0e7fab9.jpg"
-file3 = "https://telegra.ph/file/90552395a5e96d0e7fab9.jpg"
-file4 = "https://telegra.ph/file/90552395a5e96d0e7fab9.jpg"
-file5 = "https://telegra.ph/file/90552395a5e96d0e7fab9.jpg"
-""" =======================ZEN ROBOT====================== """
+""" =======================MILNA ROBOT====================== """
+file1 = "https://telegra.ph/file/d57fc4c6128e919a0729e.jpg"
+file2 = "https://telegra.ph/file/961e1390a72f9f72c18d0.jpg"
+file3 = "https://telegra.ph/file/0ec44fbff2cc2d8e2a638.jpg"
+file4 = "https://telegra.ph/file/6ab9f1f4a1d970481b4d7.jpg"
+file5 = "https://telegra.ph/file/a24f1a71689d80ab9740c.jpg"
+""" =======================MILNA ROBOT====================== """
 
 
 @register(pattern="/myinfo")
@@ -71,7 +71,7 @@ async def callback_query_handler(event):
     try:
         boy = event.sender_id
         PRO = await bot.get_entity(boy)
-        LILIE = "POWERED BY 𝗣𝘆𝘁𝗵𝗼𝗻𝗫𝗴𝗮𝗺𝗲𝗿 \n\n"
+        LILIE = "POWERED BY 𝗚𝗪 𝗧𝗲𝗮𝗺𝘀 \n\n"
         LILIE += f"FIRST NAME : {PRO.first_name} \n"
         LILIE += f"LAST NAME : {PRO.last_name}\n"
         LILIE += f"YOU BOT : {PRO.bot} \n"
